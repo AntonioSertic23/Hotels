a65cb50d6cbd34f5f0fcdb67ef3c14958aa6cc84
Antonio <45394148+AntonioSertic23@users.noreply.github.com>
2023-03-31 12:22:13 +0200

    readme++ & brisanje starih podataka

--- README.md
+++ README.md
@@ -11,6 +11,10 @@ Tema projekta je izabrana zbog riješavanja potencijalne problematike vođenja l
 Stranica prikazuje sva naučena znanja o osnovnim web tehnologijama **HTML**, **CSS** (uz **Bootstrap 3.3.7**) i **JavaScript** (uz **jQuery**). A za bazu se koristio **Firebase**.
 \
 \
+[![My Skills](https://skills.thijs.gg/icons?i=html,css,bootstrap,js,jquery,firebase)](https://skills.thijs.gg)
+
+---
+
 Omogućeni su prijava i registracija putem firebase-a uz potpunu provjeru svih unesenih parametara kao što je na primjer provjera pravilnog formata email-a.
 \
 \
@@ -20,4 +24,8 @@ Stranica sadrži 3 vrste pregleda:
 1. Za prijavljenog korisnika koji može rezervirati sobe u hotelima.
 1. Za prijavljenog admina koji može upravljati samim hotelima. Uređivat, brisat i dodavat ih te uklanjati same rezervacije. Ima uvid u statistiku te može dodavati novog admina.
 
-[![My Skills](https://skills.thijs.gg/icons?i=html,css,bootstrap,js,jquery,firebase)](https://skills.thijs.gg)
+---
+
+### Pokretanje projekta
+
+Za pokretanje projekta potrebno je kreirati vlastitu Firebase bazu po primjeru koji možete naći u `assets/podaci.json`, te dobivene podatke za spajanje zalijepiti u `js/firebase_config.js`.
--- js/firebase_config.js
+++ js/firebase_config.js
@@ -1,12 +1,12 @@
 // ---------------------------------------------------------------------------------------------------- POVEZIVANJE NA BAZU
 
 var config = {
-  apiKey: "AIzaSyC_8WTbZD7_alS3fOAz_ruIqHiJFmnTz34",
-  authDomain: "hoteli-e070d.firebaseapp.com",
-  databaseURL: "https://hoteli-e070d.firebaseio.com",
-  projectId: "hoteli-e070d",
-  storageBucket: "hoteli-e070d.appspot.com",
-  messagingSenderId: "318178555934"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
 };
 firebase.initializeApp(config);
 
@@ -18,19 +18,19 @@ var Users = [];
 
 var oDbBoravak = oDb.ref("boravak");
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(this).scrollTop(0);
 
-  oDbUsers.on("value", function(oOdgovorPosluzitelja) {
+  oDbUsers.on("value", function (oOdgovorPosluzitelja) {
     Users = [];
-    oOdgovorPosluzitelja.forEach(function(oUserSnapshot) {
+    oOdgovorPosluzitelja.forEach(function (oUserSnapshot) {
       var sUserKey = oUserSnapshot.key;
       var oUser = oUserSnapshot.val();
       Users.push({
         userORadmin: oUser.userORadmin,
         username: oUser.username,
         email: oUser.email,
-        password: oUser.password
+        password: oUser.password,
       });
     });
     console.log(Users);
